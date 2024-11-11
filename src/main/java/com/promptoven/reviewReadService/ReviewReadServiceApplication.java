90f1e538adfbcc4a0a9ce4bdc76e8a82891b9a35
ddu2 <flskhhdf@naver.com>
2024-11-11 16:25:14 +0900

    fist commit

---
+++ src/main/java/com/promptoven/reviewReadService/ReviewReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.promptoven.reviewReadService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewReadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewReadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promptoven/reviewReadService/ReviewReadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promptoven.reviewReadService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewReadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
