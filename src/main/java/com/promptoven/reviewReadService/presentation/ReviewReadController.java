7bd702cddc39fc4b86239cc6bfd5985401cfaabe
ddu2 <flskhhdf@naver.com>
2024-11-26 16:36:12 +0900

    Fix : method 변경

--- src/main/java/com/promptoven/reviewReadService/presentation/ReviewReadController.java
+++ src/main/java/com/promptoven/reviewReadService/presentation/ReviewReadController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/member/review")
+@RequestMapping("/v1/member/reviews")
 @RequiredArgsConstructor
 public class ReviewReadController {
 
