c0fc9ca0c3c878f52a4a1b5f0435785f239dca4a
ddu2 <flskhhdf@naver.com>
2024-11-27 09:09:10 +0900

    Fix : endpoint 변경

--- src/main/java/com/promptoven/reviewReadService/presentation/ReviewReadController.java
+++ src/main/java/com/promptoven/reviewReadService/presentation/ReviewReadController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/member/review-read")
+@RequestMapping("/v1/review-read")g
 @RequiredArgsConstructor
 public class ReviewReadController {
 
