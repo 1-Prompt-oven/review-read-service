fb8e70363d8d3351f16c1484934154df46a44acc
ddu2 <flskhhdf@naver.com>
2024-11-27 09:22:56 +0900

    Fix : 오타 수정

--- src/main/java/com/promptoven/reviewReadService/presentation/ReviewReadController.java
+++ src/main/java/com/promptoven/reviewReadService/presentation/ReviewReadController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/review-read")g
+@RequestMapping("/v1/review-read")
 @RequiredArgsConstructor
 public class ReviewReadController {
 
